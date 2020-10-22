901b6af5839cf5245e290bd399e5bf4d14347923
肖庆平 <xiaoqingping@qq.com>
2020-10-22 15:38:05 +0800

    thinkphp v6多应用支持

---
+++ README.md
@@ -0,0 +1,17 @@
+# multi-app
+
+基于[think-multi-app](https://github.com/top-think/think-multi-app)开发，在原多应用基础上增加：
+1. 路由提前加载
+2. 是否Request增加是否多应用请求字段
+
+该版本可以实现在中间件中获取请求的控制器等信息
+
+## 安装
+~~~
+composer require isadmincn/multi-app
+~~~
+
+## 使用
+
+用法参考ThinkPHP6完全开发手册[多应用模式](https://www.kancloud.cn/manual/thinkphp6_0/1297876)章节。
+
---
+++ src/middleware/LoadRoute.php
@@ -0,0 +1,38 @@
+<?php
+namespace isadmin\middleware;
+
+use think\event\RouteLoaded;
+
+class LoadRoute
+{
+    public function handle($request, \Closure $next)
+    {
+        $withRoute = app()->config->get('app.with_route', true) ? function () {
+            $this->loadRoutes();
+        } : null;
+
+        app()->route->dispatch($request, $withRoute);
+
+        return $next($request);
+    }
+
+    /**
+     * 加载路由
+     * @access protected
+     * @return void
+     */
+    protected function loadRoutes(): void
+    {
+        // 加载路由定义
+        $routePath = app()->getRootPath() . 'route' . DIRECTORY_SEPARATOR;
+
+        if (is_dir($routePath)) {
+            $files = glob($routePath . '*.php');
+            foreach ($files as $file) {
+                include $file;
+            }
+        }
+
+        app()->event->trigger(RouteLoaded::class);
+    }
+}
