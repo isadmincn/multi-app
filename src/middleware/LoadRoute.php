c0bdbdbc1c864afbbe4f9cce87efc2b553fa8595
肖庆平 <xiaoqingping@qq.com>
2020-10-27 00:43:15 +0800

    加载路由

--- src/middleware/LoadRoute.php
+++ src/middleware/LoadRoute.php
@@ -7,8 +7,33 @@ class LoadRoute
 {
     public function handle($request, \Closure $next)
     {
-        app()->route->dispatch($request);
+        $withRoute = app()->config->get('app.with_route', true) ? function () {
+            $this->loadRoutes();
+        } : null;
+
+        app()->route->dispatch($request, $withRoute);
 
         return $next($request);
     }
+
+    /**
+     * 加载路由
+     * @access protected
+     * @return void
+     */
+    protected function loadRoutes(): void
+    {
+        // 加载路由定义
+        $routePath = app()->http->getRoutePath();
+
+        if (is_dir($routePath)) {
+            $files = glob($routePath . '*.php');
+            foreach ($files as $file) {
+                include $file;
+            }
+        }
+
+        app()->event->trigger(RouteLoaded::class);
+    }
+
 }
